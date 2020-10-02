ffd78247261a6b481b157e1c5d8860890d3e4e04
Lizza2103 <51029873+Lizza2103@users.noreply.github.com>
2020-10-02 15:28:10 +0200

    Update ttt.js

--- ttt.js
+++ ttt.js
@@ -5,7 +5,9 @@ Array mit Buchstaben (X, O)
 var playerIcon = ['X', 'O'];
 
 var activePlayer;
-activePlayer = 1;
+activePlayer = 0;
+
+var gameActive = 0;
 
 var felder = {
     feld1: document.getElementById('feld-1'),
@@ -19,9 +21,18 @@ var felder = {
     feld9: document.getElementById('feld-9')
 };
 
+if(document.querySelector('.player-panel-0').textContent==="Winner!" || document.querySelector('.player-panel-1').textContent==="Winner!") {
+    console.log("Teeeeeeeeeeeeeeeeeeeeeeeest");
+    gameActive = 1;
+}
+
+if(gameActive === 0) {
+    console.log('Spiel ist aktiv!');
+
+
+
 function alleFelder(event) {
-        console.log(event.target);
-        var zielFeld = event.target;
+        var zielFeld = event.target; //Abfrage, wo das Event ausgelöst wurde
         //For Loop für jedes Feld: bei Klick entweder turn textcontent to X (Player 0) oder turn textcontent to O (Player 1)
         for (feld in felder) {
             
@@ -37,21 +48,59 @@ function alleFelder(event) {
             }
             
         }
-        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        winnerQuery();
         // change active Player
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        
         document.querySelector('.player-panel-0').classList.toggle('active');
         document.querySelector('.player-panel-1').classList.toggle('active');
     };
 
-   document.getElementById('felder').addEventListener('click', alleFelder); 
+    document.getElementById('felder').addEventListener('click', alleFelder);  
      
-    /*//Funktion change active Player
-    function nextPlayer() {
-       activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-       // change active Player
-       document.querySelector('.player-0-panel').classList.toggle('active');
-       document.querySelector('.player-1-panel').classList.toggle('active');
-    };
+   
     //Funktionsabfrage, ob eine Reihe voll ist, sonst nächster Spieler*/
+    function winnerQuery() {
+        if(felder.feld1.textContent === playerIcon[0] && felder.feld2.textContent === playerIcon[0] && felder.feld3.textContent === playerIcon[0]) {
+            document.querySelector('.player-panel-0').textContent="Winner!";
+        } else if(felder.feld4.textContent === playerIcon[0] && felder.feld5.textContent === playerIcon[0] && felder.feld6.textContent === playerIcon[0]) {
+            document.querySelector('.player-panel-0').textContent="Winner!";
+        } else if(felder.feld7.textContent === playerIcon[0] && felder.feld8.textContent === playerIcon[0] && felder.feld9.textContent === playerIcon[0]) {
+            document.querySelector('.player-panel-0').textContent="Winner!";
+        } else if(felder.feld1.textContent === playerIcon[0] && felder.feld4.textContent === playerIcon[0] && felder.feld7.textContent === playerIcon[0]) {
+            document.querySelector('.player-panel-0').textContent="Winner!";
+        } else if(felder.feld2.textContent === playerIcon[0] && felder.feld5.textContent === playerIcon[0] && felder.feld8.textContent === playerIcon[0]) {
+            document.querySelector('.player-panel-0').textContent="Winner!";
+        } else if(felder.feld3.textContent === playerIcon[0] && felder.feld6.textContent === playerIcon[0] && felder.feld9.textContent === playerIcon[0]) {
+            document.querySelector('.player-panel-0').textContent="Winner!";
+        } else if(felder.feld1.textContent === playerIcon[0] && felder.feld5.textContent === playerIcon[0] && felder.feld9.textContent === playerIcon[0]) {
+            document.querySelector('.player-panel-0').textContent="Winner!";
+        } else if(felder.feld3.textContent === playerIcon[0] && felder.feld5.textContent === playerIcon[0] && felder.feld7.textContent === playerIcon[0]) {
+            document.querySelector('.player-panel-0').textContent="Winner!";
+        } else if(felder.feld1.textContent === playerIcon[1] && felder.feld2.textContent === playerIcon[1] && felder.feld3.textContent === playerIcon[1]) {
+            document.querySelector('.player-panel-1').textContent="Winner!";
+        } else if(felder.feld4.textContent === playerIcon[1] && felder.feld5.textContent === playerIcon[1] && felder.feld6.textContent === playerIcon[1]) {
+            document.querySelector('.player-panel-1').textContent="Winner!";
+        } else if(felder.feld7.textContent === playerIcon[1] && felder.feld8.textContent === playerIcon[1] && felder.feld9.textContent === playerIcon[1]) {
+            document.querySelector('.player-panel-1').textContent="Winner!";
+        } else if(felder.feld1.textContent === playerIcon[1] && felder.feld4.textContent === playerIcon[1] && felder.feld7.textContent === playerIcon[1]) {
+            document.querySelector('.player-panel-1').textContent="Winner!";
+        } else if(felder.feld2.textContent === playerIcon[1] && felder.feld5.textContent === playerIcon[1] && felder.feld8.textContent === playerIcon[1]) {
+            document.querySelector('.player-panel-1').textContent="Winner!";
+        } else if(felder.feld3.textContent === playerIcon[1] && felder.feld6.textContent === playerIcon[1] && felder.feld9.textContent === playerIcon[1]) {
+            document.querySelector('.player-panel-1').textContent="Winner!";
+        } else if(felder.feld1.textContent === playerIcon[1] && felder.feld5.textContent === playerIcon[1] && felder.feld9.textContent === playerIcon[1]) {
+            document.querySelector('.player-panel-1').textContent="Winner!";
+        } else if(felder.feld3.textContent === playerIcon[1] && felder.feld5.textContent === playerIcon[1] && felder.feld7.textContent === playerIcon[1]) {
+            document.querySelector('.player-panel-1').textContent="Winner!";
+        }
+
+        else {
+            console.log('Laaaaaaangweilig!');
+        }
+        
+    };
     
-    
+} else {
+    console.log('Spiel ist nicht aktiv!');
+}
